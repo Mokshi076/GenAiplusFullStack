550d35035448c2e99485b96dad1976ea45c41746
Mokshi <mokshi104rajak@gmail.com>
2026-04-02 11:23:56 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+
+const app = require("./app.js")
+const connectToDB = require("./config/database")
+
+connectToDB()
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+
+})
